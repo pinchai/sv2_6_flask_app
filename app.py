241a361fa98fed39b7737f48009b47f453f6bfaa
pinchai <pinchai.pc@gmail.com>
2026-05-13 08:58:30 +0700

    a

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
 from flask import Flask, render_template
-from  product import product
+from product import product
 
 app = Flask(__name__)
 
 
 @app.get('/')
+@app.get('/home')
 def home():
-    return render_template('front/home.html')
+    return render_template('front/index.html')
+
+
+@app.get('/products')
+def products():
+    return render_template('front/products.html')
+
 
 @app.get('/product')
 def product():
-    return "product page"
-
-
-@app.get('/product-detail')
-def product_detail():
-    return "product detail page"
+    return render_template('front/product.html')
 
 
 @app.get('/cart')
 def cart():
-    return "cart page"
+    return render_template('front/cart.html')
 
 
 @app.get('/checkout')
 def checkout():
-    return "checkout page"
+    return render_template('front/checkout.html')
+
+
+@app.get('/account')
+def account():
+    return render_template('front/account.html')
+
+
+@app.get('/forgot-password')
+def forgot_password():
+    return render_template('front/forgot-password.html')
+
+
+@app.get('/login')
+def login():
+    return render_template('front/login.html')
+
+
+@app.get('/create-user')
+def create_user():
+    return render_template('front/create-user.html')
 
 
 if __name__ == '__main__':
